4d277cfd066579200630001454074105cf879629
Paige Power <paige.power2@gmail.com>
2018-04-20 16:22:27 -0400

    initial commit

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,41 @@
-const ohHey = "Hello World";
+let ul = document.getElementById('list');
+let removeAll = document.getElementById('removeAll');
+let add = document.getElementById('add');
 
-console.log(ohHey);
+// add on click event to add button
+// anonymous function
+add.onclick = () => {
+    addLi(ul);
+};
+
+// function fires after clicking add
+function addLi(targetUi) {
+    let inputText = document.getElementById('text').value;
+    let li = document.createElement('li');
+    let textNode = document.createTextNode(inputText + " ");
+    let removeButton = document.createElement('button');
+    document.getElementById('text').value = " ";
+
+    if (inputText.length === 0) {
+        alert ("there is no input");
+        return false;
+    }
+
+    removeButton.className = "removeMe";
+    removeButton.innerHTML = "Done!";
+    removeButton.setAttribute("onclick", "removeMe(this);");
+
+    li.appendChild(textNode);
+    li.appendChild(removeButton);
+
+    targetUi.appendChild(li);
+}
+
+function removeMe(item) {
+    let parent = item.parentElement;
+    parent.parentElement.removeChild(parent);
+}
+
+removeAll.onclick = function() {
+    ul.innerHTML = "";
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,43 @@
-"use strict";
+'use strict';
 
-var ohHey = "Hello World";
+var ul = document.getElementById('list');
+var removeAll = document.getElementById('removeAll');
+var add = document.getElementById('add');
 
-console.log(ohHey);
+// add on click event to add button
+// anonymous function
+add.onclick = function () {
+    addLi(ul);
+};
+
+// function fires after clicking add
+function addLi(targetUi) {
+    var inputText = document.getElementById('text').value;
+    var li = document.createElement('li');
+    var textNode = document.createTextNode(inputText + " ");
+    var removeButton = document.createElement('button');
+    document.getElementById('text').value = " ";
+
+    if (inputText.length === 0) {
+        alert("there is no input");
+        return false;
+    }
+
+    removeButton.className = "removeMe";
+    removeButton.innerHTML = "Done!";
+    removeButton.setAttribute("onclick", "removeMe(this);");
+
+    li.appendChild(textNode);
+    li.appendChild(removeButton);
+
+    targetUi.appendChild(li);
+}
+
+function removeMe(item) {
+    var parent = item.parentElement;
+    parent.parentElement.removeChild(parent);
+}
+
+removeAll.onclick = function () {
+    ul.innerHTML = "";
+};
